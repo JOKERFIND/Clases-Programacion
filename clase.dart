9d73f52e692b7707c76a5d6a5f887859672c3dd8
JOKERFIND <76765497+JOKERFIND@users.noreply.github.com>
2022-10-22 15:19:04 -0500

    Add files via upload

---
+++ clase.dart
@@ -0,0 +1,132 @@
+void main1() {
+
+  /* num numero = 1;
+  print(numero);
+  numero=1.1;         
+  print(numero);      
+  */
+
+  /*
+  dynamic numero = 1;
+  print(numero);
+  numero = 1.1;      
+  print(numero);     
+  numero = "hola";   
+  print(numero);
+  */
+
+  //interpolación de cadenas
+
+  /*
+  String nombre = "Juan";
+  String apellido = "Perez";
+  String nombreCompleto = "$nombre $apellido";
+  print(nombreCompleto is String); 
+  print(nombreCompleto.runtimeType); 
+  */
+  //sistemas numericos
+  /*
+  print(120.toRadixString(2)); 
+  print(120.toRadixString(8)); 
+  print(120.toRadixString(16));
+  */
+
+  //listas
+  /*
+  var mylist=[1,"hola",3.2,true,5];
+  print(mylist);
+  mylist.add("adios"); 
+  print(mylist);
+  mylist.remove("hola"); 
+  print(mylist);
+  mylist.removeAt(0); 
+  print(mylist);
+  */
+
+  //sets
+
+  /*
+  var myset = {1,2,3,4,5,6,7,8,9,10};
+  print(myset);
+  myset.add(3); //add es un método que agrega un elemento al set
+  print(myset);
+  */
+
+
+// void main () {
+//   var usuario1 = user(); //instancia de la clase user
+//   usuario1._nombre="Alex";
+//   usuario1._edad=50;
+//   usuario1.reporte();
+// }
+// 
+// //encapsulamiento
+// //- ocultar los atributos de la clase
+// //- hacerlos locales dentro de la clase
+// //- el sistema 
+// 
+// class user {
+//  //propiedades
+//  string? _nombre;
+//  int? _edad;
+//  //Metodos
+//  void reporte(){
+//   print(_nombre);
+//   print(_edad);
+//  }
+// }
+// 
+// void main(List<String>args){
+//  user usuario1=user();
+//  usuario1.nombre="Alex";
+//  usuario1.edad=15;
+//  //print(usuario1.nombre);
+//  usuario1.reporte();
+// }
+// 
+// class user{
+//  String? _nombre;
+//  int? _edad;
+// 
+//  void set nombre(String nombre) {
+//   _nombre = nombre ;
+//  }
+// 
+// void set edad(int edad) -> _edad=edad;
+// 
+// String get nombre ->_nombre!;
+// 
+// string get edad->_edad!;
+// 
+//  void reporte(){
+//   print(_nombre);
+//   print(_edad);
+//  }
+// }
+// 
+// 
+// 
+// //Scope de variables o ambito de variables
+// x=35;
+// void main(List<String>args){
+//  var x=5;
+//  void shownumber(){
+//   var y=10;
+//   print(y);
+//   print(x);
+//  }
+// showX1();
+//  shownumber();
+// }
+// 
+// //funciones fat arrow, arrow
+// void showX1(){
+//  print(x);
+// }
+// 
+// void showX2-> print(x);
+// 
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// 
+
+}
